09a7ecc3d844453f15efcc804b2dd9597118c6a2
AvinandanRoy <abhinandanroylipi@gmail.com>
2024-11-23 15:57:53 +0600

    Maveb wrapper clean , validate , test , site , deploy complete

---
+++ Maven_Wrapping/src/main/java/com/avinandan/maven_wrapping/MavenWrappingApplication.java
@@ -0,0 +1,13 @@
+package com.avinandan.maven_wrapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenWrappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenWrappingApplication.class, args);
+	}
+
+}
---
+++ Maven_Wrapping/src/test/java/com/avinandan/maven_wrapping/MavenWrappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avinandan.maven_wrapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenWrappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
